05171972173722fe648a13e3044b70bbe97a70a7
Satrya Sabeni <satrya@Satryas-MacBook-Air.local>
2022-12-10 19:28:25 +0100

    d10 p2

--- day_10.py
+++ day_10.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pickle as pkl
+
 with open('data/day_10ex.txt') as handle:
     lines = [line[:-1] for line in handle.readlines()]
 
@@ -6,8 +9,10 @@ with open('data/day_10.txt') as handle:
 
 X = 1
 intervals = [20, 60, 100, 140, 180, 220]
-interval_results = []
 cycles = 0
+crt = 0
+
+image = np.zeros((1, 240), dtype='uint8')
 
 for line in lines:
 
@@ -15,21 +20,36 @@ for line in lines:
         for cycle in range(1):
             cycles += 1
 
-            if cycles in intervals:
-                interval_val = intervals[intervals.index(cycles)] * X
-                interval_results.append(interval_val) 
+            if crt % 40 in [X-1, X, X+1]:
+                image[0][crt] = 1
+                print(X, cycles)
+
+            
+            crt += 1
+
+            # if cycles in intervals:
+            #     interval_val = intervals[intervals.index(cycles)] * X
+            #     interval_results.append(interval_val) 
 
     else:
         for cycle in range(2):
             cycles += 1
 
-            if cycles in intervals:
-                interval_val = intervals[intervals.index(cycles)] * X
-                interval_results.append(interval_val) 
+            if crt % 40 in [X-1, X, X+1]:
+                image[0][crt] = 1
+                print(X, cycles)
+
+            
+            crt += 1
+
+            # if cycles in intervals:
+            #     interval_val = intervals[intervals.index(cycles)] * X
+            #     interval_results.append(interval_val) 
         
         val = int(line.split()[1])
         X += val
+    
+with open('./message.pkl', 'wb') as handle:
+    pkl.dump(image, handle)
 
-
-print(sum(interval_results))
 print('done')
