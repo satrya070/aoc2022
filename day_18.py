cbb310dd2ca05b9df99f2e2f733dfa26cdc72350
Satrya Sabeni <satrya@Satryas-MacBook-Air.local>
2023-04-14 23:29:28 +0200

    d18 part 1

--- day_18.py
+++ day_18.py
@@ -4,20 +4,25 @@ example0 = [
 ]
 
 example = [
-    [2,2,2],
-    [1,2,2],
-    [3,2,2],
-    [2,1,2],
-    [2,3,2],
-    [2,2,1],
-    [2,2,3],
-    [2,2,4],
-    [2,2,6],
-    [1,2,5],
-    [3,2,5],
-    [2,1,5],
-    [2,3,5]
+    (2,2,2),
+    (1,2,2),
+    (3,2,2),
+    (2,1,2),
+    (2,3,2),
+    (2,2,1),
+    (2,2,3),
+    (2,2,4),
+    (2,2,6),
+    (1,2,5),
+    (3,2,5),
+    (2,1,5),
+    (2,3,5)
 ]
+
+with open('data/day_18.txt') as f:
+    lines = f.readlines()
+    lines = [eval(line[:-1]) for line in lines]
+
 
 def is_adjacent(block_a, block_b):
     x1, y1, z1 = block_a
@@ -29,7 +34,7 @@ def is_adjacent(block_a, block_b):
     if (x2, y2, z2) in horizontal_adjacents:
         surface_id = horizontal_adjacents_ids[horizontal_adjacents.index((x2, y2, z2))]
         block_surfaces[block_a][surface_id] = False
-        return True
+        return
 
     # check vertical adjacent ----------------------------------
     vertical_adjacents = [(x1, y1, z1+1), (x1, y1, z1-1)]
@@ -38,28 +43,35 @@ def is_adjacent(block_a, block_b):
     if (x2, y2, z2) in vertical_adjacents:
         surface_id = vertical_adjacents_ids[vertical_adjacents.index((x2, y2, z2))]
         block_surfaces[block_a][surface_id] = False
-        return True
+        return
     
     return False
 
-blocks_todo = example0
+blocks_todo = lines
 
 # generate dict with all surface for each block
 block_surfaces = {}
 for block in blocks_todo:
     # left: 0, up: 1, right: 2, down: 3, top: 4, bottom: 5
-    block_surfaces[block] = [True, True, True, True, True, True]
+    block_surfaces[block] = [
+        True, True, True, True, True, True
+    ]
 
-
+# go through all blocks and check if they are adjacent and update surfaces dict
 for main, block in enumerate(blocks_todo):
     for sub, sub_block in enumerate(blocks_todo):
         if main == sub:
             continue
 
-        print(main, sub)
-
         adjacent = is_adjacent(block, sub_block)
-        print(adjacent)
-    
-x = is_adjacent((1, 1, 1), (2, 1, 1))
-print(x)
+
+# count all uncovered surfaces
+uncovered_surfaces = 0
+for block, surfaces in block_surfaces.items():
+    uncovered_surfaces += surfaces.count(True)
+
+print(uncovered_surfaces)
+print('done!')
+
+# x = is_adjacent((1, 1, 1), (2, 1, 1))
+
