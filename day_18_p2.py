715d2742e78eb3baa845cf493995c752fee5bcc0
satrya070 <satrya@spotr.ai>
2023-04-18 17:24:23 +0200

    make water,air,stone division

--- day_18_p2.py
+++ day_18_p2.py
@@ -19,7 +19,7 @@ with open('data/day_18.txt') as f:
     lines = [eval(line[:-1]) for line in lines]
 
 
-def is_adjacent(block_a, block_b, block_dict):
+def is_adjacent(block_a, block_b, block_dict, checkid):
     x1, y1, z1 = block_a
     x2, y2, z2 = block_b
 
@@ -28,9 +28,9 @@ def is_adjacent(block_a, block_b, block_dict):
     horizontal_adjacents_ids = [0, 1, 2, 3]
     if (x2, y2, z2) in horizontal_adjacents:
         surface_id = horizontal_adjacents_ids[horizontal_adjacents.index((x2, y2, z2))]
-        block_dict[block_a][surface_id] = False
+        block_dict[block_a][surface_id] = checkid
         
-        return block_dict
+        return
 
     # check vertical adjacent ----------------------------------
     vertical_adjacents = [(x1, y1, z1+1), (x1, y1, z1-1)]
@@ -38,13 +38,13 @@ def is_adjacent(block_a, block_b, block_dict):
 
     if (x2, y2, z2) in vertical_adjacents:
         surface_id = vertical_adjacents_ids[vertical_adjacents.index((x2, y2, z2))]
-        block_dict[block_a][surface_id] = False
+        block_dict[block_a][surface_id] = checkid
         
-        return block_dict
+        return
     
-    return block_dict
+    return
 
-blocks_todo = lines # lines
+stone_blocks = example # lines
 
 # generate dict with all surface for each block
 # block_surfaces = {}
@@ -70,30 +70,40 @@ blocks_todo = lines # lines
 
 # part 2 ---------------------------------------------------------
 
-all_adjacent_blocks = {}
+air_blocks = {}
 
 # get all possible adjacent blocks position
-for block in blocks_todo:
-    x, y, z = block
+for stone_block in stone_blocks:
+    x, y, z = stone_block
     adjacent_blocks = [
         (x-1, y, z), (x, y+1, z), (x+1, y, z), (x, y-1, z),
         (x, y, z+1), (x, y, z-1)
     ]
 
-    for adjacent_block in adjacent_blocks:
-        if adjacent_block not in all_adjacent_blocks and adjacent_block not in blocks_todo:
-            all_adjacent_blocks[adjacent_block] = [True, True, True, True, True, True]
+    # collect all air blocks := if an adjacent block and not a real block
+    for air_block in adjacent_blocks:
+        
+        if air_block not in air_blocks and air_block not in stone_blocks:
+            air_blocks[air_block] = [0, 0, 0, 0, 0, 0]
 
-# check all adjacent blocks with actual blocks
-for main, block in enumerate(all_adjacent_blocks):
-    for sub, sub_block in enumerate(blocks_todo):
-        all_adjacent_blocks = is_adjacent(block, sub_block, all_adjacent_blocks)
+# check all air blocks with stone blocks: stone_id = 1
+for main, air_block in enumerate(air_blocks):
+    for sub, stone_block in enumerate(stone_blocks):
+        is_adjacent(air_block, stone_block, air_blocks, 1)
 
-waterfilled = sum([6 for pos, uncovered in all_adjacent_blocks.items() if not any(uncovered)])
+print("checkout stone surfaces")
 
-# print(uncovered_surfaces - waterfilled)
-print('done!')
+# check which airblocks are adjacent to each other --------
+for main, air_block in enumerate(air_blocks):
+    for sub, other_air_block in enumerate(air_blocks):
+        is_adjacent(air_block, other_air_block, air_blocks, 2)
+
+print('checked out air surfaces')
+
+# for adjacent_airblock, check which one theyre connected to, and isolated ones
+# group id these blocks
+# requirement: all blocks within a group must have air or block surfaces
+# otherwise air is flowing out. I should
+
 
 # 3326 too high 3320
-
-# check double droplet situation
